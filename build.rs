39924cffb2a60ecae82917d4494c0093ee186891
Ell <alan@cattes.us>
2020-08-01 12:49:26 -0500

    Initial commit

---
+++ build.rs
@@ -0,0 +1,31 @@
+extern crate cc;
+
+use std::env;
+
+fn main() {
+    println!("{}", env::consts::OS);
+
+    let mut builder = cc::Build::new();
+
+    builder.files(vec!["src/tray.h"]);
+
+    #[cfg(target_os = "windows")]
+    {
+        builder.define("TRAY_WINAPI", Some("1"));
+    }
+
+    #[cfg(target_os = "linux")]
+    {
+        extern crate pkg_config;
+        pkg_config::Config::new().probe("appindicator3");
+
+        builder.define("TRAY_APPINDICATOR", Some("1"));
+    }
+
+    #[cfg(target_os = "macos")]
+    {
+        builder.define("TRAY_APPKIT", Some("1"));
+    }
+
+    builder.compile("tray");
+}
